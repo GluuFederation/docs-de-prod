7593bb7104c07187e6e2fd234895bb9b7c9cd31b
jschristie <jschristiein@gmail.com>
2017-04-19 20:16:50 +0530

    updated site & - updated build

--- 3.0.0/updatesite.sh
+++ 3.0.0/updatesite.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 git pull
+mkdocs build
+rm -rf alpha/
+mv site/ alpha/
 echo -n "Enter task Performed >"
 read text
 echo "Entered Task: $text"
 
 git add -A
 git commit -m "updated site & - $text"
-git push origin master
+git push origin 3.0.0
